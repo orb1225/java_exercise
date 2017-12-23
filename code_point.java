c4babf1fb0137dd6a604931b32172d542bc6a1de
cjy <cjy_1225@163.com>
2017-12-23 22:43:24 +0800

    test

---
+++ code_point.java
@@ -0,0 +1,11 @@
+public class code_point{
+public static void main(String[]args){
+String greeting="Hello";
+int cpCount=greeting.codePointCount(0,greeting.length());
+System.out.println(cpCount);
+
+int a=0x1D546;
+System.out.println(Integer.valueOf(a));
+
+}
+}
